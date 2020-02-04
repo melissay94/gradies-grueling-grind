ecb65c0662406e92a1be998a18c011414f2c358c
Melissa Young <melissay94@yahoo.com>
2020-02-03 21:04:16 -0800

    Successfully added Phaser 3 as a dependency using Parcel

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
+"use strict";
+
+import Phaser from "phaser";
+
 let canvas = null;
-let ctx = null;
+let game = null;
+
 
 document.addEventListener("DOMContentLoaded", () => {
-    setUpCanvas();
+    canvas = document.getElementById("gradie-window");
+    game = new Phaser.Game(phaserConfig());
 });
 
-const setUpCanvas = () => {
-    canvas = document.getElementById("gradie-window");
-    ctx  =game.getContext("2d");
-    game.setAttribute("width", getComputedStyle(game)["width"]);
-    game.setAttribute("height", getComputedStyle(game)["height"]);
-}
+const phaserConfig = () => {
+    return {
+        width: getComputedStyle(canvas)["width"],
+        height: getComputedStyle(canvas)["height"],
+        parent: "gradie-window",
+        title: "Gradie's Grueling Grind",
+        version: "0.0.1",
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        },
+        fps: 30
+    };
+};
+
+const preload = () => {};
+const create = () => {};
+const update = () => {};
